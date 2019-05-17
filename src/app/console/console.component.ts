1440ecd32a41967f51133f15c38e63d9f58988db
Brian <bcchildr@us.ibm.com>
2019-05-16 23:19:04 -0500

    Added end point to environments, created a get statement on console, created a path for app-routing

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { PokemonService } from "../services/pokemon.service";
+import { NgModel } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-console",
@@ -8,11 +10,10 @@ import { PokemonService } from "../services/pokemon.service";
 })
 export class ConsoleComponent implements OnInit {
   private pokemonData: any;
-  constructor(private svc: PokemonService) {}
+  constructor(private svc: PokemonService, private http: HttpClient) {}
 
   ngOnInit() {
-    this.svc.getPokemons().subscribe(data => {
-      this.pokemonData = data;
-    });
+    let obs = this.http.get("https://pokeapi.co/api/v2/pokemon/{name}");
+    obs.subscribe(res => console.log(res));
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    url: "https://pokeapi.co/api/v2/pokemon/"
+  }
 };
